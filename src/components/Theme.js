fd12edab6a688b8f9f30fa666da6d1bba8012c5d
woz07 <muhsameer9@gmail.com>
2025-04-03 03:15:32 +0100

    add: better comments

--- src/components/Theme.js
+++ src/components/Theme.js
@@ -1,18 +1,18 @@
-// Just for clarity, this file is responsible for updating the theme of the application based on the weather icon provided by OpenWeather API.
+// just for clarity, this file is responsible for updating the theme of the application based on the weather icon provided by OpenWeather API.
 function update(icon) {
-    if (!icon) return; // Check if icon is defined
+    if (!icon) return; // check if icon is defined
 
-    // openWeather icon format: "01d" (day) or "01n" (night)
+    // open weather icon format: "01d" (day) or "01n" (night)
     var isDay = icon.includes("d"); // OpenWeather icon format: "01d" (day) or "01n" (night)
-    // change CSS variables based on day or night
+    // change index.css  root variables based on day or night
     if (isDay) {
         // apply day theme
-        document.documentElement.style.setProperty('--primary-bg-color', '#25b3f1'); // Light Sky Blue (for day)
-        document.documentElement.style.setProperty('--secondary-bg-color', '#76d3fc'); // Light Sky Blue (for day)
+        document.documentElement.style.setProperty('--primary-bg-color', '#25b3f1');
+        document.documentElement.style.setProperty('--secondary-bg-color', '#76d3fc');
     } else {
         // apply night theme
-        document.documentElement.style.setProperty('--primary-bg-color', '#05161d'); // Dark Slate Gray (for night)
-        document.documentElement.style.setProperty('--secondary-bg-color', '#172931'); // Gold (for night elements)
+        document.documentElement.style.setProperty('--primary-bg-color', '#05161d');
+        document.documentElement.style.setProperty('--secondary-bg-color', '#172931');
     }
 }
 
